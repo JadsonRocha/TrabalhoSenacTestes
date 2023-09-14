0b9b454d8ba72b98b0211b38a910ba2a6eb30834
fcostaf <fcostaf@yahoo.com.br>
2023-09-14 11:18:57 -0300

    .

--- src/Controller/MensagemController.java
+++ src/Controller/MensagemController.java
@@ -4,14 +4,29 @@
  * and open the template in the editor.
  */
 package Controller;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 /**
  *
  * @author 771000343
  */
 public class MensagemController {
-    public String buscaMensagem(int aleatorio){
-        String mensagem="";
-        return mensagem;
+    public String buscaMensagem(int aleatorio) throws IOException {
+        //BufferedReader buffRead = new BufferedReader(new FileReader("Mensagem.txt"));
+        String linha = Files.readAllLines(Paths.get("Mensagem.txt")).get(aleatorio);
+        /*
+        while (true) {
+                if (linha != null) {
+                        System.out.println(linha);
+
+                } else
+                        break;
+                linha = buffRead.readLine();
+        }
+        buffRead.close();*/
+        return linha;
     }
 }

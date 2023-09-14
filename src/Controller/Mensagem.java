ce5776e34b2fc75fc137830bea42bd96a340afda
fcostaf <fcostaf@yahoo.com.br>
2023-09-14 11:05:13 -0300

    .

--- src/Controller/Mensagem.java
+++ src/Controller/Mensagem.java
@@ -5,10 +5,27 @@
  */
 package Controller;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * @author 771000343
  */
 public class Mensagem {
-    
+    public String buscaMensagem(int aleatorio) throws IOException {
+        BufferedReader buffRead = new BufferedReader(new FileReader("Mensagem.txt"));
+        String linha = "";
+        while (true) {
+                if (linha != null) {
+                        System.out.println(linha);
+
+                } else
+                        break;
+                linha = buffRead.readLine();
+        }
+        buffRead.close();
+        return linha;
+    }
 }
